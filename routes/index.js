ac8a5b98ca19751595881cdfd39ace1b1956adbe
The Mai <Mai.The.09@gmail.com>
2018-08-13 17:40:13 -0500

    add default view route

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,15 @@ const apiRoutes = require("./api");
 // Sets API Routes and path to be www.url.com/api/etc.
 router.use("/api", apiRoutes);
 
+
+/***********************|
+|*  SET UP VIEW ROUTE  *|
+|***********************/
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 /***********|
 |* EXPORTS *| 
 |***********/
